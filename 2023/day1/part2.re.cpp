465c4711c6817c1e964a94bdff51907dc049eef8
ProbablyButter <probablybutter@gmail.com>
2023-12-01 01:24:43 -0800

    cleaner solution which does reverse parser

--- 2023/day1/part2.re.cpp
+++ 2023/day1/part2.re.cpp
@@ -22,12 +22,10 @@ re2c:yyfill:check = 1;
 #include <unordered_set>
 #include <vector>
 
-int parse_digits(const std::string &line) {
+int parse_forward(const std::string &line) {
   const char *YYCURSOR = line.data();
   const char *YYMARKER;
   const char *digit_start;
-  int first_val = -1;
-  int curr_val = 0;
   int tmp_val = 0;
   /*!stags:re2c format = 'const char *@@;'; */
 parse_start:
@@ -35,70 +33,6 @@ parse_start:
 @digit_start [1-9] {
 tmp_val = digit_start[0] - '0';
 goto process_val;
-}
-"oneight" {
-  if (first_val == -1)
-  {
-    first_val = 1;
-  }
-  curr_val = 8;
-goto parse_start;
-}
-"threeight" {
-  if (first_val == -1)
-  {
-    first_val = 3;
-  }
-  curr_val = 8;
-goto parse_start;
-}
-"fiveight" {
-  if (first_val == -1)
-  {
-    first_val = 5;
-  }
-  curr_val = 8;
-goto parse_start;
-}
-"nineight" {
-  if (first_val == -1)
-  {
-    first_val = 9;
-  }
-  curr_val = 8;
-goto parse_start;
-}
-"twone" {
-  if (first_val == -1)
-  {
-    first_val = 2;
-  }
-  curr_val = 1;
-goto parse_start;
-}
-"sevenine" {
-  if (first_val == -1)
-  {
-    first_val = 7;
-  }
-  curr_val = 9;
-goto parse_start;
-}
-"eightwo" {
-  if (first_val == -1)
-  {
-    first_val = 8;
-  }
-  curr_val = 2;
-goto parse_start;
-}
-"eighthree" {
-  if (first_val == -1)
-  {
-    first_val = 8;
-  }
-  curr_val = 3;
-goto parse_start;
 }
 "one" {
   tmp_val = 1;
@@ -137,15 +71,7 @@ goto parse_start;
   goto process_val;
 }
 [\x00] {
-  if (first_val <= 0 || first_val > 9)
-  {
-    throw std::runtime_error("invalid first val");
-  }
-  if (curr_val <= 0 || curr_val > 9)
-  {
-    throw std::runtime_error("invalid last val");
-  }
-  return first_val * 10 + curr_val;
+  goto done;
 }
 * {
   goto parse_start;
@@ -154,14 +80,75 @@ goto parse_start;
   goto done;
 
 process_val:
-  if (first_val <= 0) {
-    first_val = tmp_val;
-    curr_val = tmp_val;
-  } else {
-    curr_val = tmp_val;
-  }
+  return tmp_val;
+
+done:
+  // shouldn't get here
+  throw std::runtime_error("done");
+  return 0;
+}
+
+int parse_backward(const std::string &line) {
+  std::string rline(line.rbegin(), line.rend());
+  const char *YYCURSOR = rline.data();
+  const char *YYMARKER;
+  const char *digit_start;
+  int tmp_val = 0;
+  /*!stags:re2c format = 'const char *@@;'; */
+parse_start:
+  /*!re2c
+@digit_start [1-9] {
+tmp_val = digit_start[0] - '0';
+goto process_val;
+}
+"eno" {
+  tmp_val = 1;
+  goto process_val;
+}
+"owt" {
+  tmp_val = 2;
+  goto process_val;
+}
+"eerht" {
+  tmp_val = 3;
+  goto process_val;
+}
+"ruof" {
+  tmp_val = 4;
+  goto process_val;
+}
+"evif" {
+  tmp_val = 5;
+  goto process_val;
+}
+"xis" {
+  tmp_val = 6;
+  goto process_val;
+}
+"neves" {
+  tmp_val = 7;
+  goto process_val;
+}
+"thgie" {
+  tmp_val = 8;
+  goto process_val;
+}
+"enin" {
+  tmp_val = 9;
+  goto process_val;
+}
+[\x00] {
+  goto done;
+}
+* {
   goto parse_start;
-  
+}
+*/
+  goto done;
+
+process_val:
+  return tmp_val;
+
 done:
   // shouldn't get here
   throw std::runtime_error("done");
@@ -178,7 +165,7 @@ int main(int argc, char **argv) {
     if (in.eof()) {
       break;
     }
-    int tmp = parse_digits(line);
+    int tmp = parse_forward(line) * 10 + parse_backward(line);
     sum += tmp;
   }
   std::cout << sum << std::endl;
