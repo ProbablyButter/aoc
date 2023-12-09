0111b6a1c726e0a0f997d25098c232f7a799aa25
ProbablyButter <probablybutter@gmail.com>
2023-12-08 21:46:09 -0800

    cleanup

--- 2023/day9/part1.re.cpp
+++ 2023/day9/part1.re.cpp
@@ -24,74 +24,10 @@ re2c:yyfill:check = 1;
 #include <unordered_set>
 #include <vector>
 
-struct game {
-  int id;
-  std::set<int> winners;
-  std::vector<int> vals;
-};
-
-void parse_game(const std::string &line, std::vector<game> &games) {
-  const char *YYCURSOR = line.data();
-  const char *YYMARKER;
-  const char *digit_start;
-  const char *color_start;
-  const char *color_stop;
-  int first_val = -1;
-  int curr_val = 0;
-  int id = 0;
-  /*!stags:re2c format = 'const char *@@;'; */
-parse_start:
-  /*!re2c
-"Card" " "* @digit_start [0-9]+ ": " {
-  games.emplace_back();
-  games.back().id = atoi(digit_start);
-  goto parse_winners;
-}
-[\x00] {
-  return;
-}
-* {
-  goto parse_start;
-}
-*/
-
-parse_winners:
-  /*!re2c
-@digit_start [0-9]+ {
-  games.back().winners.insert(atoi(digit_start));
-  goto parse_winners;
-}
-"|" {
-  goto parse_card;
-}
-[\x00] {
-  return;
-}
-* {
-  goto parse_winners;
-}
-   */
-
-parse_card:
-  /*!re2c
-@digit_start [0-9]+ {
-  games.back().vals.push_back(atoi(digit_start)); goto parse_card;
-}
-[\x00] {
-  return;
-}
-* {
-  goto parse_card;
-}
-   */
-  return;
-}
-
 int main(int argc, char **argv) {
   std::filesystem::path in_path = get_resource_path("input.txt");
   std::ifstream in(in_path);
   std::string line;
-  std::vector<game> games;
   std::vector<std::vector<int64_t>> board;
   while (true) {
     std::getline(in, line);
