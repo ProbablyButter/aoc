7143ed8b7f251d886586b34ea0603a1787fa5874
ProbablyButter <probablybutter@gmail.com>
2023-12-13 23:04:11 -0800

    stubs for previous years
    solved 2015/day1

---
+++ 2015/day1/part1.re.cpp
@@ -0,0 +1,46 @@
+/*!re2c re2c:flags:utf-8 = 1;*/
+/*!max:re2c*/
+/*!re2c
+re2c:define:YYCTYPE = char;
+re2c:yyfill:enable = 0;
+re2c:flags:tags = 1;
+re2c:yyfill:check = 1;
+*/
+
+#include "aoc.hpp"
+#include "graph_tools.hpp"
+#include "string_tools.hpp"
+
+#include <algorithm>
+#include <charconv>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <regex>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+int main(int argc, char **argv) {
+  std::filesystem::path in_path = get_resource_path("input.txt");
+  std::ifstream in(in_path);
+  std::string line;
+  std::getline(in, line);
+  int64_t floor = 0;
+  for (size_t i = 0; i < line.size(); ++i) {
+    auto v = line[i];
+    switch (v) {
+    case '(':
+      ++floor;
+      break;
+    case ')':
+      --floor;
+      break;
+    }
+  }
+  std::cout << floor << std::endl;
+}
