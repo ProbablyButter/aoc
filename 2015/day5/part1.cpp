375323381971357966718570cfbe3705bdf18212
ProbablyButter <probablybutter@gmail.com>
2023-12-14 20:19:22 -0800

    solved 2015 day 5

--- 2015/day5/part1.cpp
+++ 2015/day5/part1.cpp
@@ -27,32 +27,64 @@ re2c:yyfill:check = 1;
 #include <vector>
 
 int main(int argc, char **argv) {
-  std::string input = "iwrupvqb";
-  int64_t num = 0;
+  std::filesystem::path in_path = get_resource_path("input.txt");
+  std::ifstream in(in_path);
+  std::string line;
 
+  size_t nice_count = 0;
   while (true) {
-    std::stringstream str;
-    str << input << num;
-    aoc::md5 hash;
-    hash.init();
-    std::string tmp = str.str();
-    hash.append(reinterpret_cast<const uint8_t *>(tmp.data()), tmp.size());
-    hash.finalize();
-    std::string digest = hash.str();
-    bool valid = true;
-    for (int i = 0; i < 5; ++i) {
-      if (digest[i] != '0') {
-        valid = false;
-        break;
-      }
-    }
-    if (valid) {
+    std::getline(in, line);
+    if (in.eof()) {
       break;
     }
-    ++num;
-    if ((num & 0xfffff) == 0) {
-      std::cout << num << std::endl;
+    int vowel_count = 0;
+    bool repeat_letter = false;
+    bool bad_str = false;
+    for (size_t i = 0; i < line.size(); ++i) {
+      switch (line[i]) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        ++vowel_count;
+        break;
+      default:
+        break;
+      }
+      if (i + 1 < line.size()) {
+        if (line[i] == line[i + 1]) {
+          repeat_letter = true;
+        }
+        switch (line[i]) {
+        case 'a':
+          if (line[i + 1] == 'b') {
+            bad_str = true;
+          }
+          break;
+        case 'c':
+          if (line[i + 1] == 'd') {
+            bad_str = true;
+          }
+          break;
+        case 'p':
+          if (line[i + 1] == 'q') {
+            bad_str = true;
+          }
+          break;
+        case 'x':
+          if (line[i + 1] == 'y') {
+            bad_str = true;
+          }
+          break;
+        default:
+          break;
+        }
+      }
+    }
+    if (vowel_count >= 3 && repeat_letter && !bad_str) {
+      ++nice_count;
     }
   }
-  std::cout << num << std::endl;
+  std::cout << nice_count << std::endl;
 }
--- 2015/day5/part2.cpp
+++ 2015/day5/part2.cpp
@@ -27,29 +27,45 @@ re2c:yyfill:check = 1;
 #include <vector>
 
 int main(int argc, char **argv) {
-  std::string input = "iwrupvqb";
-  int64_t num = 0;
+  std::filesystem::path in_path = get_resource_path("input.txt");
+  std::ifstream in(in_path);
+  std::string line;
 
+  size_t nice_count = 0;
   while (true) {
-    std::stringstream str;
-    str << input << num;
-    aoc::md5 hash;
-    hash.init();
-    std::string tmp = str.str();
-    hash.append(reinterpret_cast<const uint8_t *>(tmp.data()), tmp.size());
-    hash.finalize();
-    std::string digest = hash.str();
-    bool valid = true;
-    for (int i = 0; i < 6; ++i) {
-      if (digest[i] != '0') {
-        valid = false;
-        break;
-      }
-    }
-    if (valid) {
+    std::getline(in, line);
+    if (in.eof()) {
       break;
     }
-    ++num;
+
+    bool aba = false;
+    std::unordered_multimap<std::string, int64_t> char_pairs;
+    for (size_t i = 0; i < line.size(); ++i) {
+      if (i + 1 < line.size()) {
+        char_pairs.emplace(line.substr(i, 2), i);
+      }
+      if (i + 2 < line.size()) {
+        if (line[i] == line[i + 2]) {
+          aba = true;
+        }
+      }
+    }
+    bool dups = false;
+    for (auto &v : char_pairs) {
+      auto range = char_pairs.equal_range(v.first);
+      while (range.first != range.second) {
+        int64_t diff = std::abs(v.second - range.first->second);
+        if (diff > 1) {
+          dups = true;
+          goto done;
+        }
+        ++range.first;
+      }
+    }
+  done:
+    if (aba && dups) {
+      ++nice_count;
+    }
   }
-  std::cout << num << std::endl;
+  std::cout << nice_count << std::endl;
 }
